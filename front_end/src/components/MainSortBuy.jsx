2ec5e9d7206f13afe5304fc735a2999dd88c703f
bold ariunbold <ariunboldbold200@gmail.com>
2024-02-23 22:49:25 +0800

    blame ariuka lol

--- front_end/src/components/MainSortBuy.jsx
+++ front_end/src/components/MainSortBuy.jsx
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 export const MainSortBuy = () => {};
-=======
-export const MainSortBuy = (props) => {
-  const { data } = props;
-  console.log("data", data);
-  return <div>hello </div>;
-};
->>>>>>> main
