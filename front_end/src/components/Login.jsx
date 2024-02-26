bd278630b1313db6278e92642c1be8772b2ab762
Дамдинням Ганхуяг <HP230457@G303-CC0152.local>
2024-02-26 17:33:59 +0800

    mains

--- front_end/src/components/Login.jsx
+++ front_end/src/components/Login.jsx
@@ -6,7 +6,7 @@ export const Login = (props) => {
   return (
     <div className="w-[400px] h-[600px] bg-[#F06742] flex flex-col gap-4 py-2 rounded-xl">
       <div className="w-full text-white  flex flex-row-reverse px-2 ">
-        <Image src="ban.svg" height={20} width={20} />
+        <Image onClick={() => relogin()} src="ban.svg" height={20} width={20} />
       </div>
       <div className="text-black w-full flex justify-center bg-[red]">
         <Image src="logo.svg" height={60} width={60} />
--- front_end/src/pages/index.js
+++ front_end/src/pages/index.js
@@ -40,6 +40,7 @@ export default function Home() {
   const LoginButtonPress = () => {
     setLoginStat(!loginStat);
   };
+
   useEffect(() => {
     fetchChocolateData();
   }, []);
