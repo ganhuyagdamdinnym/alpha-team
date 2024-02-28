ed6537401655c23aaf49209475748eb235171dc0
Түмэн-өлзий Нарандаваа <HP230887@G303-CC0157.local>
2024-02-28 20:34:42 +0800

    tumee

--- front_end/src/components/HomeAboutCompany.jsx
+++ front_end/src/components/HomeAboutCompany.jsx
@@ -5,19 +5,15 @@ export const HomeAboutCompany = () => {
     <div className="flex gap-[40vh] justify-around p-[10vh]">
       <div>
         <Image src="/logo.svg" height={100} width={100} />
-        <p className="border-t-2 border-black ">Ritter sport</p>
+        <p className="border-t-2 border-black ">RITTER SPORT</p>
       </div>
       <div>
-        <p>Information</p>
+        <p>Contact</p>
         <p>CEO: Jargalmaa Zorigoo</p>
         <p>Premium Sweets LLC</p>
         <p>Tel: +976 7777-3555</p>
         <p>Mobile: +976 9978-8822</p>
         <p>E-mail: Jargalmaa@premiumsweets.mn</p>
-        <p>
-          Address: Suite 202, ICC Tower, 1st khoroo, Sukhbaatar district,
-          Ulaanbaatar, Mongolia
-        </p>
       </div>
       <div className="flex-col gap-[10vh]">
         <input className="w-10 h-10" placeholder="sfhjfgkag" />
