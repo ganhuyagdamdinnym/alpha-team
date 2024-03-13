f66e75f970da7f111a321632131033deea4d65f9
Түмэн-өлзий Нарандаваа <HP230887@G303-CC0157.local>
2024-03-13 19:33:44 +0800

    commit

--- front_end/src/pages/companyAbout.js
+++ front_end/src/pages/companyAbout.js
@@ -1,11 +1,5 @@
-// import { Buysort } from "../components/Buysort";
-// import { useState, useEffect } from "react";
 import { data } from "autoprefixer";
 import Image from "next/image";
-// import axios from "axios";
-// import { useRouter } from "next/navigation";
-// import { Hoppet } from "../components/Hoppet";
-// import { UserHead } from "@/components/UserHead";
 export default function About() {
   //   <div className="flex flex-wrap">
   //   <div className="flex flex-col text-wrap">
@@ -38,42 +32,41 @@ export default function About() {
   // </div>
   const data = [
     {
-      bg_image: "/ritter-history.1-removebg-preview.png",
+      bg_image: "/ritter-history.1-removebg-preview (1).png",
       story:
         "Алфред Евген Риттер, Клара Риттер нар шоколад, чихрийн үйлдвэрийг үүсгэн байгуулж, Риттер Спортын шоколадны түүхийн суурь чулууг 1912 онд Штутгарт-Бад Каннштат хотод тавьжээ.",
-      title: "Үүсэл",
+      title: "Риттер Спортын шоколадны үүсэл",
       year: "1912",
     },
     {
-      bg_image: "/Name21-removebg-preview.png",
+      bg_image: "/Name21-removebg-preview (1).png",
       story:
         "Клара Риттерийн санал: Спорт хүрэм бүрийн халаасанд хагарахгүй багтах, жирийн урт баар шиг жинтэй шоколад хийцгээе. Энэ нь Ritter's Sport Schokolade нэрээр худалдаанд гарсан.",
       title: "Дөрвөлжин шокаладний үүсэл",
       year: "1932",
     },
     {
-      bg_image: "xconvert.com-1.jpg",
+      bg_image: "/xconvert.com-1 (1).jpg",
       story:
         "Шоколадны зах зээл дээрх хувьсгал: бизнес эрхлэгч Альфред Отто Риттер Бунте Палетт (өнгөлөг төрөл) -ийг танилцуулав. Зар сурталчилгааны зурагт хуудаснууд дээр Бүх зүйл илүү өнгөлөг, илүү аз жаргалтай, илүү орчин үеийн, илүү идэвхтэй болж байна, үүнд дагалддаг шоколад орно гэж бичсэн байдаг.",
-      title: "Нэр төрлөө нэмэгдүүлсэн",
+      title: "Нэр төрлөө нэмэгдүүлсэн нь",
       year: "1974",
     },
   ];
   return (
     <div className="bg-white w-100% h-100%">
-      <h1 className="text-center text-[6vh]">Товч түүх</h1>
-      <div className="flex justify-evenly flex-wrap">
+      <div className="flex flex-wrap">
         {data.map((dta) => (
-          <div className="w-1/5 h-[60vh] min-w-200 border-2">
-            <div
-              style={{
-                backgroundImage: `url("${dta?.bg_image}")`,
-              }}
-              className="w-[40vh] h-[35vh] bg-cover bg-center"
-            ></div>
-            <p className="text-[5vh]">{dta.year}</p>
-            <p className="text-[2.5vh]">{dta.title}</p>
-            <p>{dta.story}</p>
+          <div className="w-screen h-screen gap-[10vh]">
+            <p className="text-[8vh] justify-center text-center">{dta.year}</p>
+            <div className="flex justify-around gap-[10vh]">
+              <p className="text-[5vh] text-wrap w-[2vh]">{dta.title}</p>
+              <img
+                src={dta?.bg_image}
+                className="w-[60vh] h-[60vh] ml-[40vh]"
+              />
+              <p className="w-[50vh] text-[2vh]">{dta.story}</p>
+            </div>
           </div>
         ))}
       </div>
