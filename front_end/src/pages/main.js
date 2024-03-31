7ee295f32b13957217f97b8ee9d236aacad516d9
bold ariunbold <ariunboldbold200@gmail.com>
2024-03-31 22:21:19 +0800

    shin huvilbar

--- front_end/src/pages/main.js
+++ front_end/src/pages/main.js
@@ -4,14 +4,14 @@ import { MainChocolateSort } from "@/components/MainChocolateSort";
 import MainPageGlitchFix from "@/components/Sha2";
 export default function Main() {
   return (
-    // <div className="w-[100vw]">
-    //   <MainPageGlitchFix />
-    // </div>
-
-    <div className="overflow-scroll">
-      <div className="w-[100vw] h-[100vh] max-[1000px]:h-[auto]">
-        <MainChocolateSort />
-      </div>
+    <div className="w-[100vw]">
+      <MainPageGlitchFix />
     </div>
+
+    // <div className="overflow-scroll">
+    //   <div className="w-[100vw] h-[100vh] max-[1000px]:h-[auto]">
+    //     <MainChocolateSort />
+    //   </div>
+    // </div>
   );
 }
