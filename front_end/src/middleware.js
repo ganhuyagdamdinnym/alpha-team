05fad2dd6a94a850f81d6e415678cf21120fe9c2
Дамдинням Ганхуяг <HP230457@G302-CC0093.local>
2024-04-16 19:15:37 +0800

    later fix clerk

--- front_end/src/middleware.js
+++ front_end/src/middleware.js
@@ -1,5 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
-export default authMiddleware({ publicRoutes: ["/buy", "/"] });
+export default authMiddleware({ publicRoutes: ["/main", "/companyAbout","/"], });
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/buy", "/(api|trpc)(.*)"],
 };
